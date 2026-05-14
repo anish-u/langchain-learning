b73000fdf005f6ba6eea48ad39aecb0afaefe0ce
Anish Ummenthala <ummenthala.anish@gmail.com>
2026-05-13 18:45:10 -0700

    langchain prompts and messages

---
+++ langchain-prompts/chatbot.py
@@ -0,0 +1,21 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+model = ChatOpenAI()
+
+chat_history = [
+    SystemMessage(content="You are a helpful assistant."),
+]
+
+while True:
+    user_input = input("You: ")
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == "exit":
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI: ", result.content)
+
+print("Chat: ", chat_history)
